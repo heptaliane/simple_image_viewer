b677ca0398bf5ee90dd21747008465be2a332b31
heptaliane <heptaliane@gmail.com>
2024-01-18 00:22:16 +0900

    Enable window.__TAURI__.event.listen

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,12 @@
+use wasm_bindgen::prelude::*;
 use yew::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "event"])]
+    async fn listen(event: &str, handler: &Closure<dyn FnMut(JsValue)>) -> JsValue;
+}
+
 
 #[function_component]
 pub fn App() -> Html {
