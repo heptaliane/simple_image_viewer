fe5cded65653591aeb43de75e80530569b94eb6f
heptaliane <heptaliane@gmail.com>
2024-01-18 00:20:33 +0900

    Add send image uri emit

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,22 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
+mod payload;
+
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            app.emit_to(
+                "main",
+                "image_uri",
+                payload::ImageURIPayload {
+                    uri: "public/tauri.svg".into(),
+                },
+            ).unwrap();
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src-tauri/src/payload.rs
@@ -0,0 +1,4 @@
+#[derive(Clone, serde::Serialize)]
+pub struct ImageURIPayload {
+    pub uri: String,
+}
