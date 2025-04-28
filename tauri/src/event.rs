35e217e9c7dc7c1b6ea7544f60c1769619e4130b
heptaliane <heptaliane@gmail.com>
2025-04-28 17:18:13 +0900

    Fix legacy event handler

--- tauri/src/event.rs
+++ tauri/src/event.rs
@@ -1,13 +1,13 @@
 use std::sync::{Arc, Mutex};
 
-use tauri::{self, Manager};
+use tauri::{self, Manager, Emitter};
 
 use crate::path::PathProvider;
 use shared::event::TauriEvent;
 use shared::payload::ImagePayload;
 
 pub fn receive_image(app_handle: &tauri::AppHandle, images: &Arc<Mutex<PathProvider>>) {
-    let main_window = app_handle.get_window("main").unwrap();
+    let main_window = app_handle.get_webview_window("main").unwrap();
 
     if let Some(img) = images.lock().unwrap().get() {
         main_window
--- tauri/src/main.rs
+++ tauri/src/main.rs
@@ -3,6 +3,8 @@
 use std::sync::{Arc, Mutex};
 
 use shared::event::TauriEvent;
+use tauri::Listener;
+use tauri_plugin_cli::CliExt;
 use tauri::Manager;
 
 mod event;
@@ -12,7 +14,7 @@ fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .setup(|app| {
-            let args = app.get_cli_matches().unwrap().args;
+            let args = app.cli().matches().unwrap().args;
 
             let filenames: Vec<String> = args["filenames"]
                 .value
@@ -26,7 +28,7 @@ fn main() {
             {
                 let images = images.clone();
                 let app_handle = app.app_handle();
-                let _ = app.listen_global(TauriEvent::RequestImage.as_ref(), move |_| {
+                app.listen(TauriEvent::RequestImage.as_ref(), move |_| {
                     event::receive_image(&app_handle, &images);
                 });
             }
@@ -34,7 +36,7 @@ fn main() {
             {
                 let images = images.clone();
                 let app_handle = app.app_handle();
-                let _ = app.listen_global(TauriEvent::MoveNext.as_ref(), move |_| {
+                app.listen(TauriEvent::MoveNext.as_ref(), move |_| {
                     images.lock().unwrap().move_next();
                     event::receive_image(&app_handle, &images);
                 });
@@ -43,7 +45,7 @@ fn main() {
             {
                 let images = images.clone();
                 let app_handle = app.app_handle();
-                let _ = app.listen_global(TauriEvent::MovePrev.as_ref(), move |_| {
+                app.listen(TauriEvent::MovePrev.as_ref(), move |_| {
                     images.lock().unwrap().move_prev();
                     event::receive_image(&app_handle, &images);
                 });
