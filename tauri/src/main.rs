18a70e7166d9f25fe17fa6f4a439bb1913c7cbcf
heptaliane <heptaliane@gmail.com>
2025-04-28 19:33:11 +0900

    Remove outdates modules

--- tauri/src/main.rs
+++ tauri/src/main.rs
@@ -1,56 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use std::sync::{Arc, Mutex};
 
-use shared::event::TauriEvent;
-use tauri::Listener;
-use tauri_plugin_cli::CliExt;
-use tauri::Manager;
+use tauri::generate_handler;
 
-mod event;
-mod path;
+mod command;
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .setup(|app| {
-            let args = app.cli().matches().unwrap().args;
-
-            let filenames: Vec<String> = args["filenames"]
-                .value
-                .as_array()
-                .unwrap()
-                .iter()
-                .map(|filename| filename.as_str().unwrap().to_string())
-                .collect();
-            let images = Arc::new(Mutex::new(path::PathProvider::new(&filenames)));
-
-            {
-                let images = images.clone();
-                let app_handle = app.app_handle();
-                app.listen(TauriEvent::RequestImage.as_ref(), move |_| {
-                    event::receive_image(&app_handle, &images);
-                });
-            }
-
-            {
-                let images = images.clone();
-                let app_handle = app.app_handle();
-                app.listen(TauriEvent::MoveNext.as_ref(), move |_| {
-                    images.lock().unwrap().move_next();
-                    event::receive_image(&app_handle, &images);
-                });
-            }
-
-            {
-                let images = images.clone();
-                let app_handle = app.app_handle();
-                app.listen(TauriEvent::MovePrev.as_ref(), move |_| {
-                    images.lock().unwrap().move_prev();
-                    event::receive_image(&app_handle, &images);
-                });
-            }
-
+        .invoke_handler(generate_handler![command::get_files])
+        .setup(|_app| {
             Ok(())
         })
         .run(tauri::generate_context!())
